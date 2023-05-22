0fe4d2f0ab625984c6b0b7d65560a40314457991
Karo <karolina.stachowiak0@gmail.com>
2023-05-22 19:22:09 +0200

    Extract javascript to a seperate file

---
+++ karo.js
@@ -0,0 +1,9 @@
+function leftScroll() {
+    const left = document.querySelector(".row_2");
+    left.scrollBy(1500, 0);
+}
+
+function rightScroll() {
+    const right = document.querySelector(".row_2");
+    right.scrollBy(-1500, 0);
+}
